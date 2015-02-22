63b59ca0f582f4a8bee71605b7d0363ae89e2ac3
scp44 <scp44@case.edu>
2015-02-22 12:11:18 -0500

    Menus now in C#
    All the menus are what they were before, but now in C# instead of
    javascript.

---
+++ TopDownShooter/Assets/MenuButtonCSharp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonCSharp : MonoBehaviour {
+
+	public void StartGame () {
+		Application.LoadLevel("GunSelect");
+	}
+	
+	public void ExitGame () {
+		Application.Quit();
+	}
+}
---
+++ TopDownShooter/Assets/gunSelectCSharp.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gunSelectCSharp : MonoBehaviour {
+	int [] selected;
+	// Use this for initialization
+	void Start () {
+		selected = new int[3];
+		selected [0] = 0;
+		selected [1] = 0;
+		selected [2] = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame () {
+		Application.LoadLevel("ArenaGen");
+	}
+	
+	public void MainMenuReturn () {
+		Application.LoadLevel("StartMenu");
+	}
+	
+	void GunSelect(int gun) {
+		if(gun == selected[0])
+		{
+			selected[0] = 0;
+			//unhighlight button?
+		}
+		
+		else if(gun == selected[1])
+		{
+			selected[1] = 0;
+			//unhighlight button?
+		}
+		
+		else
+		{
+			if(selected[0] == 0)
+			{
+				selected[0] = gun;
+			}
+			
+			else if(selected[1] == 0)
+			{
+				selected[1] = gun;
+			}
+		}
+	}
+	
+	void StandardDisplay () {
+		
+	}
+	
+	void SpeedDisplay () {
+		
+	}
+	
+	void AoEDisplay () {
+		
+	}
+	
+	void SprayerDisplay () {
+		
+	}
+	
+	void LongDisplay () {
+		
+	}
+}
