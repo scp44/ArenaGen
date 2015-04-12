686cad8b0908d411d9cc3bf166b85d6fb402a917
scp44 <scp44@case.edu>
2015-04-12 15:12:07 -0400

    Fixed explosion destruction
    Now destroys on a timer rather than when it hits something.

--- TopDownShooter/Assets/Scripts/GameEngine/ExplosionScript.cs
+++ TopDownShooter/Assets/Scripts/GameEngine/ExplosionScript.cs
@@ -4,25 +4,34 @@ using System.Collections;
 public class ExplosionScript : MonoBehaviour {
 	public float damage = 1;
 	public Vector3 startPosition;
+	private int timer;
+	private int lifespan = 3;
 	// Use this for initialization
 	void Start () {
-	
+		timer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		timer++;
+		if (timer >= lifespan)
+			Destroy (this.gameObject);
 	}
 
 	void OnTriggerEnter(Collider other){
 		//run enemy health minus
-		EnemyBasic enemyScript = other.GetComponent<EnemyBasic>();
-		enemyScript.takeDamage (damage);
-		Destroy(this.gameObject);
-		//if enemy is not firing, look at player
-		if (!enemyScript.isFiring) {
-			Vector3 bulletDirection = this.transform.position - startPosition;
-			other.transform.rotation = Quaternion.LookRotation(-bulletDirection);
+		if (other.tag == "Enemy") {
+			EnemyBasic enemyScript = other.GetComponent<EnemyBasic> ();
+			enemyScript.takeDamage (damage);
+			if(timer >= lifespan)
+				Destroy (this.gameObject);
+			//if enemy is not firing, look at player
+			if (!enemyScript.isFiring) {
+				Vector3 bulletDirection = this.transform.position - startPosition;
+				other.transform.rotation = Quaternion.LookRotation (-bulletDirection);
+			}
+		} else {
+			Destroy (this.gameObject);
 		}
 	}
 }
